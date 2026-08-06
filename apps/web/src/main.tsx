51e20ff10e2962120c086b7fc824405ec24cd509
anacocorangers <anacocorangers@gmail.com>
2026-08-06 12:26:51 -0500

    Add Vercel Analytics to the web app

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import CurrentPage from './CurrentPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CurrentPage />
+    <Analytics />
   </StrictMode>,
 )
